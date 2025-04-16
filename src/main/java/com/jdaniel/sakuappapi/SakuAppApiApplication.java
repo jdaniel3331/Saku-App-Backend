41b7c82cdec863cd8e043252d71d1242f1dd25d4
jdaniel3331 <jdaniel10031@outlook.com>
2025-04-16 15:13:05 -0600

    chore: First commit for Saku API

---
+++ src/main/java/com/jdaniel/sakuappapi/SakuAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.jdaniel.sakuappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakuAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SakuAppApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jdaniel/sakuappapi/SakuAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdaniel.sakuappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakuAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
