46af9ebf5862d19b2e8fc9c8ee6117d1879fa324
jdaniel3331 <jdaniel10031@outlook.com>
2025-04-16 20:59:23 -0600

    feat: Test endpoint for the API

---
+++ src/main/java/com/jdaniel/sakuappapi/controller/HealthController.java
@@ -0,0 +1,21 @@
+package com.jdaniel.sakuappapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+    /**
+     * Health check endpoint.
+     *
+     * @return a simple "Hello, World!" message
+     */
+    @GetMapping
+    public String health() {
+        return "Hello, World! \n" +
+                "This is a health check endpoint for the SakuApp API.\n" +
+                "If you see this message, the API is running.";
+    }
+}
