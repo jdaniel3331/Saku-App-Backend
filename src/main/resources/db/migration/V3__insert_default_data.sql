2885e844c75aaf4e47a7854e8f5b7b00d9cade1d
jdaniel3331 <jdaniel10031@outlook.com>
2025-04-20 19:55:27 -0600

    chore: Adding default data migration

---
+++ src/main/resources/db/migration/V3__insert_default_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO task.priority_levels (level)
+VALUES
+    ('Baja'),
+    ('Media'),
+    ('Alta');
+
+INSERT INTO task.task_states (state)
+VALUES
+    ('Pendiente'),
+    ('En Progreso'),
+    ('Completada'),
+    ('En pausa');
