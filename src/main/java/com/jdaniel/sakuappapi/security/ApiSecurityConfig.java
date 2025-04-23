372892cb786701536425df66d4930f0130a0a818
jdaniel3331 <jdaniel10031@outlook.com>
2025-04-22 19:58:32 -0600

    chore: Add API security configuration with custom password encoder

--- src/main/java/com/jdaniel/sakuappapi/security/ApiSecurityConfig.java
+++ src/main/java/com/jdaniel/sakuappapi/security/ApiSecurityConfig.java
@@ -1,4 +1,19 @@
 package com.jdaniel.sakuappapi.security;
 
+import com.jdaniel.sakuappapi.util.EncryptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 public class ApiSecurityConfig {
+    @Autowired
+    private EncryptionService encryptionService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return encryptionService;
+    }
 }
