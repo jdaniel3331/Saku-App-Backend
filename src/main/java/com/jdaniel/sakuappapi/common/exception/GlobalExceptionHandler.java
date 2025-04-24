96375f3fab7a0d0f80869b974aa615c2d33740fd
jdaniel3331 <jdaniel10031@outlook.com>
2025-04-23 22:24:59 -0600

    fix: Handle invalid JSON exception with proper error response

--- src/main/java/com/jdaniel/sakuappapi/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jdaniel/sakuappapi/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.jdaniel.sakuappapi.common.exception;
 import com.jdaniel.sakuappapi.common.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,5 +13,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiResponse> handleRequestException(RequestException e) {
         ApiResponse response = new ApiResponse(e.getStatus(), e.getMessage(), e.getCode());
         return new ResponseEntity<>(response, HttpStatus.valueOf(e.getCode()));
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse> handleInvalidJsonException(MethodArgumentNotValidException ex) {
+        ApiResponse error = new ApiResponse(HttpStatus.BAD_REQUEST.name(), ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
