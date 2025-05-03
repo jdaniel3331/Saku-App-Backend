ab5247404b60024b5b4771deeb6ba4f87768595e
jdaniel3331 <jdaniel10031@outlook.com>
2025-05-02 21:27:05 -0600

    feat: Create updateTitleByTaskId method

--- src/main/java/com/jdaniel/sakuappapi/task/repository/TaskRepository.kt
+++ src/main/java/com/jdaniel/sakuappapi/task/repository/TaskRepository.kt
@@ -3,6 +3,8 @@ package com.jdaniel.sakuappapi.task.repository
 import com.jdaniel.sakuappapi.user.model.User
 import com.jdaniel.sakuappapi.task.model.Task
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 
@@ -14,4 +16,9 @@ interface TaskRepository: JpaRepository<Task, Long> {
     fun existsByTaskId(taskId: Long): Boolean
     @Transactional
     fun deleteByTaskId(taskId: Long)
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Task t SET t.title = ?1 WHERE t.taskId = ?2")
+    fun updateTitleByTaskId(title: String, taskId: Long)
 }
