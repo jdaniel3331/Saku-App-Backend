e53fbef0f545fc0e77f9f932d4b58008d0608cc5
jdaniel3331 <jdaniel10031@outlook.com>
2025-05-02 19:33:56 -0600

    feat: Allow ApiResponse to include additional data

--- src/main/java/com/jdaniel/sakuappapi/common/response/ApiResponse.java
+++ src/main/java/com/jdaniel/sakuappapi/common/response/ApiResponse.java
@@ -1,17 +1,20 @@
 package com.jdaniel.sakuappapi.common.response;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 
-public record ApiResponse(
+public record ApiResponse<T>(
         LocalDateTime timestamp,
         String status,
         String message,
-        int code
+        int code,
+        T data
 ) {
     //set date to now
     public ApiResponse(String status, String message, int code) {
-        this(LocalDateTime.now(), status, message, code);
+        this(LocalDateTime.now(), status, message, code, null);
     }
+    public ApiResponse(String status, String message, int code, T data) {
+        this(LocalDateTime.now(), status, message, code, data);
+    }
+
 }
--- src/main/java/com/jdaniel/sakuappapi/task/model/dto/TaskDto.kt
+++ src/main/java/com/jdaniel/sakuappapi/task/model/dto/TaskDto.kt
@@ -1,23 +1,3 @@
 package com.jdaniel.sakuappapi.task.model.dto
 
-import com.fasterxml.jackson.annotation.JsonProperty
-import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotNull
-import java.time.LocalDate
-
-data class TaskDto(
-    @NotNull
-    @NotBlank
-    val title: String,
-    val description: String,
-    @NotNull
-    @JsonProperty("due_date")
-    val dueDate: LocalDate,
-    val category: Long,
-    @JsonProperty("priority_level")
-    val priorityLevel: Short,
-    @NotNull
-    @JsonProperty("user_id")
-    val userId: Long
-    //TODO: Obtener el userId del Token
-)
+data class TaskDto()
