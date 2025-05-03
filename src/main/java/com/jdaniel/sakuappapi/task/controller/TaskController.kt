b526d5c0fc98519f0840468257fc57ef75610a04
jdaniel3331 <jdaniel10031@outlook.com>
2025-05-02 19:57:48 -0600

    feat: Create endpoint for get all user tasks

--- src/main/java/com/jdaniel/sakuappapi/task/controller/TaskController.kt
+++ src/main/java/com/jdaniel/sakuappapi/task/controller/TaskController.kt
@@ -1,15 +1,19 @@
 package com.jdaniel.sakuappapi.task.controller
 
 import com.jdaniel.sakuappapi.common.response.ApiResponse
+import com.jdaniel.sakuappapi.task.model.Task
+import com.jdaniel.sakuappapi.task.model.dto.CreateTaskDto
 import com.jdaniel.sakuappapi.task.model.dto.TaskDto
 import com.jdaniel.sakuappapi.task.service.TaskService
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -19,8 +23,24 @@ class TaskController {
     private val taskService: TaskService? = null
 
     @PostMapping
-    public fun createTask(@RequestBody @Valid task: TaskDto): ResponseEntity<ApiResponse> {
-        var response: ApiResponse? = ApiResponse(HttpStatus.CREATED.name,taskService?.createTask(task), HttpStatus.CREATED.value())
-        return ResponseEntity<ApiResponse>(response, HttpStatus.CREATED)
+    public fun createTask(@RequestBody @Valid task: CreateTaskDto): ResponseEntity<ApiResponse<String>> {
+        val message = taskService?.createTask(task)
+        val response = ApiResponse<String>(
+            HttpStatus.CREATED.name,
+            message,
+            HttpStatus.CREATED.value()
+        )
+        return ResponseEntity(response, HttpStatus.CREATED)
+    }
+    @GetMapping
+    public fun getAllTasks(@RequestParam("userId") userId: Long): ResponseEntity<ApiResponse<List<TaskDto>>> {
+        val tasks = taskService?.getAllTasks(userId)
+        val response = ApiResponse<List<TaskDto>>(
+            HttpStatus.OK.name,
+            "Tasks retrieved successfully",
+            HttpStatus.OK.value(),
+            tasks
+        )
+        return ResponseEntity(response, HttpStatus.OK)
     }
 }
