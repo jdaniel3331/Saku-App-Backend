de78d1c2c529a0edc697467c3e51e0befcf23e05
jdaniel3331 <jdaniel10031@outlook.com>
2025-05-02 19:54:43 -0600

    feat: Add taskId attribute to TaskDto

--- src/main/java/com/jdaniel/sakuappapi/task/model/dto/TaskDto.kt
+++ src/main/java/com/jdaniel/sakuappapi/task/model/dto/TaskDto.kt
@@ -1,3 +1,20 @@
 package com.jdaniel.sakuappapi.task.model.dto
 
-data class TaskDto()
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.time.LocalDate
+
+data class TaskDto(
+    @JsonProperty("user_id")
+    val taskId: Long,
+    val title: String,
+    val description: String?,
+    @JsonProperty("created_at")
+    val createdAt: LocalDate,
+    @JsonProperty("due_date")
+    val dueDate: LocalDate?,
+    val category: Long?,
+    @JsonProperty("task_state")
+    val taskState: Short,
+    @JsonProperty("priority_level")
+    val priorityLevel: Short,
+)
