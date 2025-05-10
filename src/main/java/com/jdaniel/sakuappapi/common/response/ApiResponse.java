6564634c25e9ba56e9cdd6892ddfbb14314ce1d8
jdaniel3331 <jdaniel10031@outlook.com>
2025-05-10 11:56:51 -0600

    feat: Format timestamp to dd-MM-yyyy HH:mm:ss

--- src/main/java/com/jdaniel/sakuappapi/common/response/ApiResponse.java
+++ src/main/java/com/jdaniel/sakuappapi/common/response/ApiResponse.java
@@ -1,20 +1,22 @@
 package com.jdaniel.sakuappapi.common.response;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public record ApiResponse<T>(
-        LocalDateTime timestamp,
+        String timestamp,
         String status,
         String message,
         int code,
         T data
 ) {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
     //set date to now
     public ApiResponse(String status, String message, int code) {
-        this(LocalDateTime.now(), status, message, code, null);
+        this(LocalDateTime.now().format(formatter), status, message, code, null);
     }
     public ApiResponse(String status, String message, int code, T data) {
-        this(LocalDateTime.now(), status, message, code, data);
+        this(LocalDateTime.now().format(formatter), status, message, code, data);
     }
 
 }
